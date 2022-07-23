e76f7455e49c1168ed34a2019723a3fafb6cd69b
mikiasendale <69900537+mikiasendale@users.noreply.github.com>
2022-07-23 10:21:39 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function cond(){
+	var a=document.getElementById("nm").value;
+	if(a=="DNIAME"){
+		window.location.href = "http://www.dniame.onrender.com/";
+	}
+	else if(a=="MEISAD"){
+		window.location.href = "http://www.meisad.onrender.com/;
+	}
+	else if(a=="AYSIKA"){
+		window.location.href = "http://www.aysika.onrender.com/;
+	}
+	else if(a=="YAIOFA"){
+		window.location.href = "http://www.yaiofa.onrender.com/;
+	}
+	else{
+		window.location.href = "http://www.404wmc.onrender.com/;
+	}
+}
+		
